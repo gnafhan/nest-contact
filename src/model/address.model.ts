496b736897331ca2765ef0486b84aff66bf9e9c5
gnafhan <nafhanghifari@gmail.com>
2024-03-23 21:58:31 +0700

    create: remove address api

--- src/model/address.model.ts
+++ src/model/address.model.ts
@@ -15,3 +15,23 @@ export class CreateAddressRequest {
   country: string;
   postal_code: string;
 }
+
+export class GetAddressRequest {
+  contact_id: number;
+  address_id: number;
+}
+
+export class RemoveAddressRequest {
+  contact_id: number;
+  address_id: number;
+}
+
+export class UpdateAddressRequest {
+  id: number;
+  contact_id: number;
+  street?: string;
+  city?: string;
+  province?: string;
+  country: string;
+  postal_code: string;
+}
